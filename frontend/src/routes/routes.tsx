4856592afe7caf954da16e2d39ab2aafd6906065
Mateus de Sousa <mateus69107@gmail.com>
2024-06-10 21:57:42 -0300

    :sparkles: Atualização de produto

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -2,10 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import { Login } from "../pages/login/login";
 import { Main } from "../pages/main/main";
 import { CreateProduct } from "../pages/create-product/create-product";
+import { UpdateProduct } from "../pages/update-product/update-product";
 
 export const Rotas = () => {
   return (
     <Routes >
+        <Route path="/update-product/:idProduct" element={<UpdateProduct/>}></Route>
         <Route path="/create-product" element={<CreateProduct/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
         <Route path='/' element={<Main/>}></Route>
